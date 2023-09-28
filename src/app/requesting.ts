6e60034c6bbeba0525f1575dcb819e8709920cf4
divyansh-ukg <divyansh.jain@ukg.com>
2023-09-28 11:54:12 +0530

    update changes for requesting blood page

--- src/app/requesting.ts
+++ src/app/requesting.ts
@@ -1,14 +1,12 @@
 export class Requesting
 {
     id:number = 0;
-    name:string = '';
-    email:string = '';
     bloodgroup:string = '';
     units:number = 0;
     disease:string = '';
     mobile:string = '';
     gender:string = '';
-    age:number = 0;
+    age:number = 18;
     status:string = 'false';
 
     constructor() {}
